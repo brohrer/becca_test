a0ae2f9d6b9b3d569c1f2e34b497ddd9df6e80ba
Brandon Rohrer <brohrer@gmail.com>
2016-07-24 19:32:00 -0400

    renamed files added

---
+++ beccatest/worlds/__init__.py
@@ -0,0 +1,7 @@
+"""
+A set of simple worlds that can be used to evaluate BECCA agents
+
+These are toy worlds, but they are diverse.
+Their purpose is to push the limits of typical learning agents
+is as simple a manner as possible.
+"""
