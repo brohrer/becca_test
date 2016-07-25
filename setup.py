35da1292b6aaf23bee684b48bc8cca2ed853c13c
Brandon Rohrer <brohrer@gmail.com>
2016-07-24 20:00:12 -0400

    installs properly

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='beccatest',
-version='0.8',
+version='0.8.1',
 description='A test suite for BECCA',
 url='http://github.com/brohrer/beccatest',
 download_url='https://github.com/brohrer/beccatest/archive/master.zip',
