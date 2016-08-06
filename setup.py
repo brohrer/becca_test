de9d7261bbbc7d252a0496fe54c33d03d182e514
Brandon Rohrer <brohrer@gmail.com>
2016-08-06 09:02:05 -0400

    Version updated to 0.8.3

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup
 
-setup(name='becca_test',
-version='0.8.2',
-description='A test suite for Becca',
-url='http://github.com/brohrer/becca_test',
-download_url='https://github.com/brohrer/becca_test/archive/master.zip',
-author='Brandon Rohrer',
-author_email='brohrer@gmail.com',
-license='MIT',
-packages=['becca_test'],
-install_requires=['becca'],
-zip_safe=False)
+setup(
+    name='becca_test',
+    version='0.8.3',
+    description='A test suite for Becca',
+    url='http://github.com/brohrer/becca_test',
+    download_url='https://github.com/brohrer/becca_test/archive/master.zip',
+    author='Brandon Rohrer',
+    author_email='brohrer@gmail.com',
+    license='MIT',
+    packages=['becca_test'],
+    include_package_data=True,
+    install_requires=['becca'],
+    zip_safe=False)
