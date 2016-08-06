f42876445d9cdf5be8f4fb82a1acf58549366db5
Brandon Rohrer <brohrer@gmail.com>
2016-08-06 07:52:17 -0400

    Version updated to 0.8.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='becca_test',
-version='0.8.1',
+version='0.8.2',
 description='A test suite for Becca',
 url='http://github.com/brohrer/becca_test',
 download_url='https://github.com/brohrer/becca_test/archive/master.zip',
