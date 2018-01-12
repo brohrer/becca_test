02a696ae8823d624041ed2da2855479044afa5b2
Seth Weidman <sethhweidman@gmail.com>
2018-01-12 14:51:15 -0500

    Commented out not installed worlds

--- becca_test/test.py
+++ becca_test/test.py
@@ -31,17 +31,17 @@ import numpy as np
 import becca.connector
 # Import the suite of test worlds
 from becca_test.grid_1D import World as World_grid_1D
-from becca_test.grid_1D_cont import World as World_grid_1D_cont
+# from becca_test.grid_1D_cont import World as World_grid_1D_cont
 from becca_test.grid_1D_chase import World as World_grid_1D_chase
-from becca_test.grid_1D_chase_cont import World as World_grid_1D_chase_cont
+# from becca_test.grid_1D_chase_cont import World as World_grid_1D_chase_cont
 from becca_test.grid_1D_delay import World as World_grid_1D_delay
-from becca_test.grid_1D_delay_cont import World as World_grid_1D_delay_cont
+# from becca_test.grid_1D_delay_cont import World as World_grid_1D_delay_cont
 from becca_test.grid_1D_ms import World as World_grid_1D_ms
-from becca_test.grid_1D_ms_cont import World as World_grid_1D_ms_cont
+# from becca_test.grid_1D_ms_cont import World as World_grid_1D_ms_cont
 from becca_test.grid_1D_noise import World as World_grid_1D_noise
 from becca_test.grid_2D import World as World_grid_2D
 from becca_test.grid_2D_dc import World as World_grid_2D_dc
-from becca_test.grid_2D_cont import World as World_grid_2D_cont
+# from becca_test.grid_2D_cont import World as World_grid_2D_cont
 from becca_test.image_1D import World as World_image_1D
 from becca_test.image_2D import World as World_image_2D
 from becca_test.fruit import World as World_fruit
@@ -54,17 +54,17 @@ def suite(lifespan=1e5):
     start_time = time.time()
     performance = []
     performance.append(test_world(World_grid_1D, lifespan=lifespan))
-    performance.append(test_world(World_grid_1D_cont, lifespan=lifespan))
+    # performance.append(test_world(World_grid_1D_cont, lifespan=lifespan))
     performance.append(test_world(World_grid_1D_chase, lifespan=lifespan))
-    performance.append(test_world(World_grid_1D_chase_cont, lifespan=lifespan))
+    # performance.append(test_world(World_grid_1D_chase_cont, lifespan=lifespan))
     performance.append(test_world(World_grid_1D_delay, lifespan=lifespan))
-    performance.append(test_world(World_grid_1D_delay_cont, lifespan=lifespan))
+    # performance.append(test_world(World_grid_1D_delay_cont, lifespan=lifespan))
     performance.append(test_world(World_grid_1D_ms, lifespan=lifespan))
-    performance.append(test_world(World_grid_1D_ms_cont, lifespan=lifespan))
+    # performance.append(test_world(World_grid_1D_ms_cont, lifespan=lifespan))
     performance.append(test_world(World_grid_1D_noise, lifespan=lifespan))
     performance.append(test_world(World_grid_2D, lifespan=lifespan))
     performance.append(test_world(World_grid_2D_dc, lifespan=lifespan))
-    performance.append(test_world(World_grid_2D_cont, lifespan=lifespan))
+    # performance.append(test_world(World_grid_2D_cont, lifespan=lifespan))
     performance.append(test_world(World_image_1D, lifespan=lifespan))
     performance.append(test_world(World_image_2D, lifespan=lifespan))
     performance.append(test_world(World_fruit, lifespan=lifespan))
@@ -73,17 +73,17 @@ def suite(lifespan=1e5):
     # Some tests are harder than others. Weight them accordingly.
     weights = np.array([
         1., # grid_1D
-        1., # grid_1D_cont
+        # 1., # grid_1D_cont
         1., # grid_1D_chase
-        1., # grid_1D_chase_cont
+        # 1., # grid_1D_chase_cont
         1., # grid_1D_delay
-        1., # grid_1D_delay_cont
+        # 1., # grid_1D_delay_cont
         1., # grid_1D_ms
-        1., # grid_1D_ms_cont
+        # 1., # grid_1D_ms_cont
         1., # grid_1D_noise
         3., # grid_2D
         4., # grid_2D_dc
-        4., # grid_2D_cont
+        # 4., # grid_2D_cont
         5., # image_1D
         10., # image_2D
         3., # fruit
