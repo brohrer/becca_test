d4f46b18ce6f7b120d9b8e1088f2baa6a6031f1e
Brandon Rohrer <brohrer@fb.com>
2018-09-06 07:11:56 -0400

    Prep for version 10 release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='becca_test',
-    version='0.10.0.0',
+    version='0.10.0',
     description='A test suite for Becca',
     url='http://github.com/brohrer/becca_test',
     download_url='https://github.com/brohrer/becca_test/archive/master.zip',
